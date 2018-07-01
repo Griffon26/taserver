6413ffc0a6005cdb63fbfa83af79a786b4347ec6
Maurice van der Pot <griffon26@kfk4ever.com>
2018-07-01 10:32:43 +0200

    Maintain playerinfo and send the correct name in chat

--- server/server.py
+++ server/server.py
@@ -6,6 +6,12 @@ from datatypes import *
 class ProtocolViolation(Exception):
     pass
 
+class PlayerInfo():
+    def __init__(self, name):
+        self.name = name
+        self.tag = ''
+        self.server = None
+
 class Server():
     def __init__(self, serverqueue, clientqueues):
         self.serverqueue = serverqueue
@@ -20,7 +26,6 @@ class Server():
               'motd' : 'Join this server to connect to a dedicated server running on the same machine as your client',
               'ip' : (127, 0, 0, 1),
               'port' : 7777,
-              'players' : {}
             },
             { 'serverid1' : 0x00000002,
               'serverid2' : 0x80000002,
@@ -28,9 +33,10 @@ class Server():
               'motd' : 'Join this server to connect to a dedicated server hosted by Griffon26',
               'ip' : taserverip,
               'port' : 7777,
-              'players' : {}
             }
         ]
+        self.players = {
+        }
 
     def findserverbyid1(self, id1):
         for serverdata in self.servers:
@@ -48,10 +54,15 @@ class Server():
         authenticated = False
         while True:
             for clientid, clientseq, requests in self.serverqueue:
+                if not clientid in self.players:
+                    self.players[clientid] = PlayerInfo('ConnectingPlayer')
+                
                 if requests == None:
                     print('server: client(%s)\'s reader quit; stopping writer' % clientid)
                     self.clientqueues[clientid].put((None, None))
                     del(self.clientqueues[clientid])
+                    del(self.players[clientid])
+                    
                 else:
 
                     def sendmsg(msg):
@@ -63,12 +74,14 @@ class Server():
                         if request.ident == 0x01bc:
                             sendmsg(a01bc())
                             sendmsg(a0197())
+                            
                         elif request.ident == 0x003a:
                             if not authenticated:
                                 authenticated = True
                                 sendmsg(a003a())
                             else:
                                 playername = request.findbytype(m0494).value
+                                self.players[clientid].name = playername
                                 sendmsg([
                                     a003d().setplayer(playername, ''),
                                     m0662(0x8898, 0xdaff),
@@ -85,35 +98,44 @@ class Server():
                                 ])
                         elif request.ident == 0x0033:
                             sendmsg(a0033())
+                            
                         elif request.ident == 0x00d5:
                             if request.findbytype(m0228).value == 1:
                                 sendmsg(originalfragment(0x1EEB3, 0x20A10)) # 00d5 (map list)
                             else:
                                 sendmsg(a00d5().setservers(self.servers))   # 00d5 (server list)
+                                
                         elif request.ident == 0x0014:
                             sendmsg(originalfragment(0x20A18, 0x20B3F)) # 0014 (class list)
+                            
                         elif request.ident == 0x018b:
                             sendmsg(originalfragment(0x20B47, 0x20B4B)) # 018b
+                            
                         elif request.ident == 0x01b5:
                             sendmsg(originalfragment(0x20B53, 0x218F7)) # 01b5 (watch now)
+                            
                         elif request.ident == 0x0176:
                             sendmsg(originalfragment(0x218FF, 0x219D1)) # 0176
                             sendmsg(originalfragment(0x28AC9, 0x2F4D7)) # 0177 (store 0218)
+                            
                         elif request.ident == 0x00b1: # server join step 1
                             serverid1 = request.findbytype(m02c7).value
                             serverdata = self.findserverbyid1(serverid1)
                             serverid2 = serverdata['serverid2']
                             sendmsg(a00b0(9).setserverid1(serverid1))
                             sendmsg(a00b4().setserverid2(serverid2))
+                            
                         elif request.ident == 0x00b2: # server join step 2
                             serverid2 = request.findbytype(m02c4).value
                             serverdata = self.findserverbyid2(serverid2)
                             sendmsg(a00b0(10))
                             sendmsg(a0035().setserverdata(serverdata))
+                            
                         elif request.ident == 0x0070: # chat
-                            request.content.append(m02fe().set('Griffon28'))
-                            request.content.append(m06de().set('tag'))
+                            request.content.append(m02fe().set(self.players[clientid].name))
+                            request.content.append(m06de().set(self.players[clientid].tag))
                             sendmsg(request)
+                            
                         else:
                             pass
 
