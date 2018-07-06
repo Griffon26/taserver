dcd8d4bcf034928b4ed4396a9f6d9812c8c96b30
Maurice van der Pot <griffon26@kfk4ever.com>
2018-07-07 00:59:42 +0200

    taserverfirewall now actually modifies firewall rules
    This requires admin

--- scripts/taserverfirewall.py
+++ scripts/taserverfirewall.py
@@ -4,6 +4,7 @@ import argparse
 import gevent
 import gevent.queue
 from gevent.server import StreamServer
+import gevent.subprocess as sp
 import re
 
 whitelistfilename = 'firewallwhitelist.txt'
@@ -34,8 +35,60 @@ def writeiplist(filename, iplist):
         for ip in sorted(iplist):
             f.write('%d.%d.%d.%d\n' % ip)
 
-def handleserver(serverqueue):
+def removeallrules():
+    args = [
+        'c:\\windows\\system32\\Netsh.exe',
+        'advfirewall',
+        'firewall',
+        'delete',
+        'rule',
+        'name="TAserverfirewall"'
+    ]
+    # Don't check for failure here, because it is expected to
+    # fail if there are no left-over rules from a previous run.
+    sp.call(args)
 
+def removewhitelistrule(ip):
+    args = [
+        'c:\\windows\\system32\\Netsh.exe',
+        'advfirewall',
+        'firewall',
+        'delete',
+        'rule',
+        'name="TAserverfirewall"',
+        'protocol=udp',
+        'dir=in',
+        'profile=Private',
+        'localport=7777',
+        'remoteip=%d.%d.%d.%d' % ip
+    ]
+    try:
+        sp.check_call(args)
+    except sp.CalledProcessError as e:
+        print('Failed to remove rule from firewall.')
+
+def addwhitelistrule(ip):
+    args = [
+        'c:\\windows\\system32\\Netsh.exe',
+        'advfirewall',
+        'firewall',
+        'add',
+        'rule',
+        'name="TAserverfirewall"',
+        'protocol=udp',
+        'dir=in',
+        'enable=yes',
+        'profile=Private',
+        'localport=7777',
+        'action=allow',
+        'remoteip=%d.%d.%d.%d' % ip
+    ]
+    try:
+        sp.check_call(args)
+    except sp.CalledProcessError as e:
+        print('Failed to add rule to firewall.')
+
+def handleserver(serverqueue):
     try:
         iplist = readiplist(whitelistfilename)
         print('Loaded IP list from %s' % whitelistfilename)
@@ -43,24 +96,27 @@ def handleserver(serverqueue):
         print('File not found. Starting with empty IP list')
         iplist = set()
 
-    # TODO: set up all the rules according to iplist
+    # Default is to block, so for a whitelist we only need to add allow rules
+    removeallrules()
+    for ip in iplist:
+        addwhitelistrule(ip)
     
     while True:
         action, ip = serverqueue.get()
 
         if action == 'add':
             if ip not in iplist:
-                # TODO: add a firewall rule
                 print('add firewall rule for', ip)
                 iplist.add(ip)
+                addwhitelistrule(ip)
         else:
             if ip in iplist:
-                # TODO: remove a firewall rule
                 print('remove firewall rule for', ip)
                 iplist.remove(ip)
+                removewhitelistrule(ip)
 
         writeiplist(whitelistfilename, iplist)
-
+       
 def main(args):
     serverqueue = gevent.queue.Queue()
     
@@ -74,7 +130,10 @@ def main(args):
                               (ipbytes[1], ipbytes[2], ipbytes[3], ipbytes[4])) )
 
     server = StreamServer(('127.0.0.1', 9801), handleclient)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        removeallrules()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
