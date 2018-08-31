515477a691a6517f8214627a32b841c6c4d2e83c
Maurice van der Pot <griffon26@kfk4ever.com>
2018-08-31 22:41:06 +0200

    Forgot to add ping handler to launcher

---
+++ game_server_launcher/pinghandler.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2018  Maurice van der Pot <griffon26@kfk4ever.com>
+#
+# This file is part of taserver
+#
+# taserver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# taserver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with taserver.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import gevent
+from gevent.server import DatagramServer
+
+
+class EchoServer(DatagramServer):
+    def handle(self, data, address):
+        self.socket.sendto(data, address)
+
+
+def handle_ping():
+    gevent.getcurrent().name = 'pinghandler'
+    EchoServer('0.0.0.0:9002').serve_forever()
