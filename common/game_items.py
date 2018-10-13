84c55503ea7d350e86135a7492b0b1bbd796c860
JosephSpearritt <joe.spearritt@live.com>
2018-10-14 10:41:17 +1100

    Add GOTY item defs

--- common/game_items.py
+++ common/game_items.py
@@ -473,6 +473,7 @@ _hierarchical_definitions_ootb = {
                         'Raider_Primary_PlasmaGun': 8251,
                         'Medium_Sidearm_NovaBlaster': 7394,
                         'Soldier_Secondary_Eagle': 7388,
+                        'Technician_Belt_MotionAlarm': 7388,
                     },
                     'other': {
                         'Soldier_Primary_AssaultRifle_MKD': 8406,
@@ -502,6 +503,8 @@ _hierarchical_definitions_ootb = {
                     'Soldier_Belt_FragGrenadeXL': 7430,
                     'Soldier_Belt_FragGrenadeXL_MKD': 8399,
                     'Soldier_Belt_ProximityGrenade': 8222,
+                    'Technician_Belt_TCNG': 7736,
+                    'Technician_Belt_TCNG_MKD': 8416,
                 },
             },
             'packs': {
@@ -597,6 +600,8 @@ _hierarchical_definitions_ootb = {
                     'Brute_Belt_FractalGrenade_MKD': 8397,
                     'Brute_Belt_LightStickyGrenade': 7455,
                     'Juggernaut_Belt_DiskToss': 7459,
+                    'Juggernaut_Pack_HealthRegen': 7831,
+                    'Juggernaut_Pack_Energy': 7901,
                 },
             },
             'packs': {
@@ -629,6 +634,295 @@ _hierarchical_definitions_ootb = {
             'Perk Safe Fall': 8162,
         },
         'other': {
+            'Perk Safety Third': 8163,
+            'Perk Reach': 7916,
+            'Perk Wheel Deal': 8169,
+            'Perk Bounty Hunter': 8153,
+            'Perk Close Combat': 8156,
+            'Perk Stealthy': 8164,
+            'Perk Super Capacitor': 8165,
+            'Perk Egocentric': 7917,
+            'Perk Pilot': 8159,
+            'Perk Super Heavy': 8166,
+            'Perk Ultra Capacitor': 8168,
+            'Perk Quickdraw': 8161,
+            'Perk Mechanic': 8170,
+            'Perk Determination': 8157,
+            'Perk Looter': 8158,
+            'Perk Potential Energy': 8160,
+            'Perk Sonic Punch': 8231,
+            'Perk Rage': 8232,
+            'Perk Lightweight': 8646,
+        },
+    },
+    'voices': {
+        'ootb': {
+            'Voice Light': 8666,
+            'Voice Medium': 8667,
+            'Voice Heavy': 8668,
+            'Voice Dark': 8669,
+            'Voice Fem1': 8670,
+            'Voice Fem2': 8671,
+            'Voice Aus': 8695,
+            'Voice T2 Fem01': 8712,
+            'Voice T2 Fem02': 8714,
+            'Voice T2 Fem03': 8715,
+            'Voice T2 Fem04': 8716,
+            'Voice T2 Fem05': 8717,
+            'Voice T2 Male01': 8719,
+            'Voice T2 Male02': 8720,
+            'Voice T2 Male03': 8721,
+            'Voice T2 Male04': 8722,
+            'Voice T2 Male05': 8723,
+            'Voice T2 Derm01': 8724,
+            'Voice T2 Derm02': 8725,
+            'Voice T2 Derm03': 8726,
+            'Voice Total Biscuit': 8747,
+            'Voice Stowaway': 8749,
+        },
+        'other': {
+            'Voice Basement Champion': 8750,  # Unreleased voice
+        },
+
+    }
+}
+
+_hierarchical_definitions_goty = {
+    'classes': {
+        'light': {
+            'weapons': {
+                'impact': {
+                    'ootb': {},
+                    'other': {
+                        'Pathfinder_Primary_LightSpinfusor': 7422,
+                        'Pathfinder_Primary_BoltLauncher': 7425,
+                        'Pathfinder_Primary_LightSpinfusor_100X': 8696,
+                        'Pathfinder_Primary_LightTwinfusor': 8245,
+                        'Pathfinder_Primary_LightSpinfusor_MKD': 8415,
+                        'Light_Primary_LightGrenadeLauncher': 8761,
+                        'Infiltrator_Primary_StealthLightSpinfusor': 7902,
+                        'Infiltrator_Primary_RemoteArxBuster': 8252,
+                        'Infiltrator_Primary_RhinoSMG': 7397,
+                        'Infiltrator_Primary_RhinoSMG_MKD': 8409,
+                        'Sentinel_Primary_SniperRifle': 7400,
+                        'Sentinel_Primary_PhaseRifle': 7395,
+                        'Sentinel_Primary_SniperRifle_MKD': 8407,
+                        'Sentinel_Primary_SAP20': 8254,
+                    },
+
+                },
+                'bullet': {
+                    'ootb': {},
+                    'other': {
+                        'Pathfinder_Secondary_LightAssaultRifle': 7438,
+                        'Pathfinder_Secondary_Shotgun': 7399,
+                        'Pathfinder_Secondary_Shotgun_MKD': 8411,
+                        'Light_Sidearm_Sparrow': 7433,
+                        'Infiltrator_Secondary_ThrowingKnives': 8256,
+                        'Infiltrator_Secondary_SN7': 7418,
+                        'Infiltrator_Secondary_SN7_MKD': 8404,
+                        'Sentinel_Secondary_Falcon': 7419,
+                        'Sentinel_Secondary_AccurizedShotgun': 8239,
+                        'All_H1_Shocklance': 7435,
+                    },
+                },
+            },
+            'belt': {
+                'ootb': {},
+                'other': {
+                    'Pathfinder_Belt_ImpactNitron': 7387,
+                    'Pathfinder_Belt_STGrenade': 7437,
+                    'Pathfinder_Belt_ImpactNitron_MKD': 8396,
+                    'Infiltrator_Belt_StickyGrenade': 7402,
+                    'Infiltrator_Belt_StickyGrenade_MKD': 8398,
+                    'Infiltrator_Belt_PrismMines': 7440,
+                    'Infiltrator_Belt_NinjaSmoke': 8248,
+                    'Sentinel_Belt_GrenadeT5': 7914,
+                    'Sentinel_Belt_Claymore': 7421,
+                    'Sentinel_Belt_ArmoredClaymore': 8240,
+                },
+            },
+            'packs': {
+                'ootb': {},
+                'other': {
+                    'Pathfinder_Pack_JumpPack': 7822,
+                    'Pathfinder_Pack_EnergyRecharge': 7825,
+                    'Infiltrator_Pack_Stealth': 7833,
+                    'Sentinel_Pack_EnergyRecharge': 7900,
+                    # 'Sentinel_Pack_DropJammer': 7456, # Repurposed as Drop Station
+                },
+            },
+            'skins': {
+                'ootb': {
+                    'Skin PTH': 7834,
+                    'Skin PTH Mercenary': 8326,
+                    'Skin INF': 7835,
+                    'Skin INF Mercenary': 8336,
+                    'Skin INF Assassin': 8665,
+                    'Skin SEN': 8327,
+                    'Skin SEN Mercenary': 8337,
+                },
+                'other': {},
+            }
+        },
+        'medium': {
+            'weapons': {
+                'impact': {
+                    'ootb': {},
+                    'other': {
+                        'Soldier_Primary_Spinfusor': 7401,
+                        'Soldier_Primary_Twinfusor': 8257,
+                        'Soldier_Primary_Honorfusor': 8768,
+                        'Soldier_Primary_AssaultRifle': 7385,
+                        'Soldier_Primary_AssaultRifle_MKD': 8406,
+                        'Raider_Primary_ArxBuster': 7384,
+                        'Raider_Primary_ArxBuster_MKD': 8391,
+                        'Raider_Primary_GrenadeLauncher': 7416,
+                        'Raider_Primary_PlasmaGun': 8251,
+                        'Technician_Primary_Thumper': 7461,
+                        'Technician_Primary_TCN4': 7443,
+                        'Technician_Primary_TCN4_MKD': 8410,
+                        'Technician_Primary_TC24': 8699,  # Repurposed as Flak Cannon
+                    },
+                },
+                'bullet': {
+                    'ootb': {},
+                    'other': {
+                        'Soldier_Secondary_SpareSpinfusor': 8697,
+                        'Soldier_Secondary_ThumperD': 7462,
+                        'Soldier_Secondary_ThumperD_MKD': 8417,
+                        'Soldier_Secondary_Eagle': 7388,
+                        'Medium_Sidearm_NovaBlaster': 7394,
+                        'Raider_Secondary_NJ4SMG': 7441,
+                        'Raider_Secondary_NJ4SMG_MKD': 8408,
+                        'Raider_Secondary_NJ5SMG': 8249,
+                        'Technician_Secondary_RepairToolSD': 7436,
+                        'Technician_Secondary_RepairToolSD_MKD': 8405,
+                        'Technician_Secondary_SawedOff': 7427,
+                        'Medium_ElfProjector': 8765,
+                        'All_H1_Shocklance': 7435,
+                    },
+                },
+            },
+            'belt': {
+                'ootb': {},
+                'other': {
+                    'Soldier_Belt_FragGrenadeXL': 7430,
+                    'Soldier_Belt_FragGrenadeXL_MKD': 8399,
+                    'Soldier_Belt_APGrenade': 7434,
+                    'Soldier_Belt_ProximityGrenade': 8222,
+                    'Raider_Belt_EMPGrenade': 7444,
+                    'Raider_Belt_WhiteOut': 7432,
+                    'Raider_Belt_MIRVGrenade': 8247,
+                    'Raider_Belt_EMPGrenade_MKD': 8395,
+                    'Technician_Belt_TCNG': 7736,
+                    'Technician_Belt_TCNG_MKD': 8416,
+                    'Technician_Belt_MotionAlarm': 7388,
+                },
+            },
+            'packs': {
+                'ootb': {},
+                'other': {
+                    'Soldier_Pack_EnergyPool': 7824,
+                    'Soldier_Pack_Utility': 8223,
+                    'Raider_Pack_Shield': 7832,
+                    'Raider_Pack_Jammer': 7827,
+                    'Sentinel_Pack_DropJammer': 7456,  # Repurposed
+                    'Technician_Pack_LightTurret': 7413,
+                    'Technician_Pack_EXRTurret': 7417,
+                },
+            },
+            'skins': {
+                'ootb': {
+                    'Skin SLD': 8328,
+                    'Skin SLD Mercenary': 8748,
+                    'Skin RDR': 8330,
+                    'Skin RDR Mercenary': 8352,
+                    'Skin RDR Griever': 8351,
+                    'Skin TCN': 8329,
+                    'Skin TCN Mercenary': 8731,
+                },
+                'other': {},
+
+            }
+        },
+        'heavy': {
+            'weapons': {
+                'impact': {
+                    'ootb': {},
+                    'other': {
+                        'Juggernaut_Primary_FusionMortar': 7393,
+                        'Juggernaut_Primary_FusionMortar_MKD': 8400,
+                        'Juggernaut_Primary_MIRVLauncher': 7457,
+                        'Doombringer_Primary_ChainGun': 7386,
+                        'Doombringer_Primary_ChainGun_MKD': 8392,
+                        'Doombringer_Primary_HeavyBoltLauncher': 7452,
+                        'Brute_Primary_HeavySpinfusor': 7448,
+                        'Brute_Primary_HeavySpinfusor_MKD': 8414,
+                        'Brute_Primary_SpikeLauncher': 8401,
+                    },
+                },
+                'bullet': {
+                    'ootb': {},
+                    'other': {
+                        'Juggernaut_Secondary_SpinfusorD': 7446,
+                        'Juggernaut_Secondary_SpinfusorD_MKD': 8413,
+                        'Juggernaut_Secondary_HeavyTwinfusor': 8656,
+                        'Juggernaut_Secondary_X1LMG': 7458,
+                        'Doombringer_Secondary_SaberLauncher': 7398,
+                        'Doombringer_Secondary_SaberLauncher_MKD': 8357,
+                        'Brute_Secondary_AutoShotgun': 7449,
+                        'Brute_Secondary_AutoShotgun_MKD': 8412,
+                        'Brute_Secondary_NovaColt': 7431,
+                        'Brute_Secondary_PlasmaCannon': 8250,
+                        'Heavy_Sidearm_NovaBlaster_MKD': 8403,
+                        'Elf_FlakCannon': 8766,
+                        'All_H1_Shocklance': 7435,
+                    },
+                },
+            },
+            'belt': {
+                'ootb': {},
+                'other': {
+                    'Juggernaut_Belt_HeavyAPGrenade': 7447,
+                    'Juggernaut_Belt_HeavyAPGrenade_MKD': 8394,
+                    'Juggernaut_Belt_DiskToss': 7459,
+                    'Doombringer_Belt_Mine': 7392,
+                    'Doombringer_Belt_FragGrenade': 7390,
+                    'Brute_Belt_FractalGrenade': 7428,
+                    'Brute_Belt_FractalGrenade_MKD': 8397,
+                    'Brute_Belt_LightStickyGrenade': 7455,
+                },
+            },
+            'packs': {
+                'ootb': {},
+                'other': {
+                    'Juggernaut_Pack_HealthRegen': 7831,
+                    'Juggernaut_Pack_Energy': 7901,
+                    'Doombringer_Pack_ForceField': 7411,
+                    'Brute_Pack_HeavyShield': 7826,
+                    'Brute_Pack_MinorEnergy': 7830,
+                    'Brute_Pack_SurvivalPack': 8255,
+                },
+            },
+            'skins': {
+                'ootb': {
+                    'Skin JUG': 8331,
+                    'Skin JUG Mercenary': 8745,
+                    'Skin DMB': 8332,
+                    'Skin DMB Mercenary': 8744,
+                    'Skin BRT': 8333,
+                    'Skin BRT Mercenary': 8663,
+                },
+                'other': {},
+            }
+        },
+    },
+    'perks': {
+        'ootb': {},
+        'other': {
+            'Perk Survivalist': 8167,
+            'Perk Safe Fall': 8162,
             'Perk Safety Third': 8163,
             'Perk Reach': 7916,
             'Perk Wheel Deal': 8169,
