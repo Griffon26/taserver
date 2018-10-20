fd3ecd72a3d448e2a9b7c26e37d6cf6986c0d55e
JosephSpearritt <joe.spearritt@live.com>
2018-10-20 21:35:34 +1100

    Use controller config within data folder

--- game_server_launcher/gameserverhandler.py
+++ game_server_launcher/gameserverhandler.py
@@ -73,7 +73,7 @@ def run_game_server(game_server_config):
     try:
         working_dir = game_server_config['dir']
         dll_to_inject = game_server_config['controller_dll']
-        dll_config_path = game_server_config.get('controller_config', None)
+        dll_config_path = game_server_config['controller_config']
     except KeyError as e:
         raise ConfigurationError("%s is a required configuration item under [gameserver]" % str(e))
 
@@ -88,7 +88,10 @@ def run_game_server(game_server_config):
 
     if not os.path.isabs(dll_to_inject):
         dll_to_inject = os.path.abspath(dll_to_inject)
+    if not os.path.isabs(dll_config_path):
+        dll_config_path = os.path.abspath(dll_config_path)
     logger.info('gameserver: Path to controller DLL is %s' % dll_to_inject)
+    logger.info('gameserver: Path to controller configuration is %s' % dll_config_path)
 
     if not os.path.exists(dll_to_inject):
         raise ConfigurationError(
