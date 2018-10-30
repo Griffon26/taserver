bea235e83d70f7fb7646090888e36bc5de0d42e1
JosephSpearritt <joe.spearritt@live.com>
2018-10-30 21:43:25 +1100

    In the default server config, prevent sniper rifles from being paired with packs other than the Light Energy Pack

--- data/controller/serverconfig.lua
+++ data/controller/serverconfig.lua
@@ -1,2 +1,17 @@
 -- TAMods-Server configuration can be placed in this file
 -- You can read about the configuration language at: https://www.tamods.org/docs/doc_srv_api_overview.html
+
+ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1", "Light", "Thrust Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1A", "Light", "Thrust Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "Phase Rifle", "Light", "Thrust Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "SAP20", "Light", "Thrust Pack")
+
+ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1", "Light", "Stealth Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1A", "Light", "Stealth Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "Phase Rifle", "Light", "Stealth Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "SAP20", "Light", "Stealth Pack")
+
+ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1", "Light", "Light Utility Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1A", "Light", "Light Utility Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "Phase Rifle", "Light", "Light Utility Pack")
+ServerSettings.MutuallyExclusiveItems.add("Light", "SAP20", "Light", "Light Utility Pack")
