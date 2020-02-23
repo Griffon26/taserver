7beda953af8bab0337e918a7c75daa3b3df0f147
Maurice van der Pot <griffon26@kfk4ever.com>
2020-02-23 19:42:44 +0100

    Add support for port offset to firewall

---
+++ common/ports.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020  Maurice van der Pot <griffon26@kfk4ever.com>
+#
+# This file is part of taserver
+#
+# taserver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# taserver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with taserver.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+class Ports:
+
+    ports = {
+        'gameserver1': 7777,     # UDP
+        'gameserver2': 7778,     # UDP
+        # gameserverNproxy    = gameserverN + 100   # UDP
+        # gameserverNfirewall = gameserverN + 200   # TCP
+        'client2login': 9000,    # TCP
+        'launcher2login': 9001,  # TCP
+        'game2launcher': 9002,   # TCP
+        'launcherping': 9002,    # UDP
+        'restapi': 9080,         # TCP
+        'firewall': 9801,        # TCP
+    }
+
+    def __init__(self, portOffset):
+        assert isinstance(portOffset, int), f'portOffset must be int, not {portOffset.__class__.__name__}'
+        self.portOffset = portOffset
+
+    def __getitem__(self, key):
+        if key in self.ports:
+            return self.ports[key] + self.portOffset
+        elif key.endswith('proxy') and key[:5] in self.ports:
+            return self.ports[key[:5]] + 100 + self.portOffset
+        elif key.endswith('firewall') and key[:5] in self.ports:
+            return self.ports[key[:5]] + 200 + self.portOffset
+        else:
+            raise KeyError(f'{key} is not a valid port descriptor.')
--- firewall/main.py
+++ firewall/main.py
@@ -18,27 +18,30 @@
 # along with taserver.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import configparser
 import gevent
 import gevent.queue
 from gevent.server import StreamServer
 import gevent.subprocess as sp
 import json
 import logging
+import os
 import sys
 
 from common.geventwrapper import gevent_spawn
 from common.logging import set_up_logging
+from common.ports import Ports
 from common.tcpmessage import TcpMessageReader
 
 from .utils import FirewallUtils
 
-GAME_PORT1 = 7777
-GAME_PORT2 = 7778
+INI_PATH = os.path.join('data', 'shared.ini')
 
 
 class Rulelist:
-    def __init__(self):
+    def __init__(self, ports):
         self.IPs = set()
+        self.ports = ports
 
     def has_ip(self, ip):
         return ip in self.IPs
@@ -59,12 +62,14 @@ class Rulelist:
 
 
 class Blacklist(Rulelist):
-    name = 'TAserverfirewall-blacklist'
 
-    def __init__(self, utils, logger):
-        super().__init__()
+    def __init__(self, utils, logger, ports):
+        super().__init__(ports)
         self.utils = utils
         self.logger = logger
+        self.name = 'TAserverfirewall-blacklist'
+        if self.ports.portOffset:
+            self.name += f'_offset{self.ports.portOffset}'
 
     def remove_all(self):
         self.utils.remove_rules_by_name(self.name)
@@ -84,7 +89,7 @@ class Blacklist(Rulelist):
             'dir=in',
             'enable=yes',
             'profile=any',
-            'localport=9000',
+            'localport=%d' % self.ports['client2login'],
             'action=allow'
         ]
         try:
@@ -95,20 +100,22 @@ class Blacklist(Rulelist):
 
     def add(self, ip):
         if super().add(ip):
-            self.utils.add_rule(self.name, ip, 9000, 'tcp', 'block')
+            self.utils.add_rule(self.name, ip, self.ports['client2login'], 'tcp', 'block')
 
     def remove(self, ip):
         if super().remove(ip):
-            self.utils.remove_rule(self.name, ip, 9000, 'tcp', 'block')
+            self.utils.remove_rule(self.name, ip, self.ports['client2login'], 'tcp', 'block')
 
 
 class Whitelist(Rulelist):
-    name = 'TAserverfirewall-whitelist'
 
-    def __init__(self, utils, logger):
-        super().__init__()
+    def __init__(self, utils, logger, ports):
+        super().__init__(ports)
         self.utils = utils
         self.logger = logger
+        self.name = 'TAserverfirewall-whitelist'
+        if self.ports.portOffset:
+            self.name += f'_offset{self.ports.portOffset}'
 
     def remove_all(self):
         self.utils.remove_rules_by_name(self.name)
@@ -120,32 +127,36 @@ class Whitelist(Rulelist):
     def add(self, ip):
         if super().add(ip):
             for protocol in ('udp', 'tcp'):
-                self.utils.add_rule(self.name, ip, '%d,%d' % (GAME_PORT1, GAME_PORT2), protocol, 'allow')
+                self.utils.add_rule(self.name, ip, '%d,%d' % (self.ports['gameserver1'], self.ports['gameserver2']), protocol, 'allow')
 
     def remove(self, ip):
         if super().remove(ip):
             for protocol in ('udp', 'tcp'):
-                self.utils.remove_rule(self.name, ip, '%d,%d' % (GAME_PORT1, GAME_PORT2), protocol, 'allow')
+                self.utils.remove_rule(self.name, ip, '%d,%d' % (self.ports['gameserver1'], self.ports['gameserver2']), protocol, 'allow')
 
 
 class Firewall:
-    def __init__(self):
+    def __init__(self, ports):
         set_up_logging('taserver_firewall.log')
         self.logger = logging.getLogger('firewall')
+        self.ports = ports
         self.utils = FirewallUtils(self.logger)
-        self.blacklist = Blacklist(self.utils, self.logger)
-        self.whitelist = Whitelist(self.utils, self.logger)
+        self.blacklist = Blacklist(self.utils, self.logger, ports)
+        self.whitelist = Whitelist(self.utils, self.logger, ports)
+        self.name = 'TAserverfirewall-general'
+        if self.ports.portOffset:
+            self.name += f'_offset{self.ports.portOffset}'
 
     def remove_all_rules(self):
         self.logger.info('Removing any previous TAserverfirewall rules')
-        self.utils.remove_rules_by_name('TAserverfirewall-general')
+        self.utils.remove_rules_by_name(self.name)
         self.blacklist.remove_all()
         self.whitelist.remove_all()
 
     def run(self, server_queue):
         lists = {
-            'whitelist' : self.whitelist,
-            'blacklist' : self.blacklist
+            'whitelist': self.whitelist,
+            'blacklist': self.blacklist
         }
 
         # First disable the rules that are created by Windows itself when you run tribesascend.exe
@@ -153,9 +164,9 @@ class Firewall:
         for program in tribes_ascend_programs:
             self.utils.disable_rules_for_program_name(program)
 
-        self.utils.add_rule('TAserverfirewall-general', 'any', 9001, 'tcp', 'allow') # for game servers
-        self.utils.add_rule('TAserverfirewall-general', 'any', 9080, 'tcp', 'allow') # for REST
-        self.utils.add_rule('TAserverfirewall-general', 'any', 9002, 'udp', 'allow') # for in-game pings
+        self.utils.add_rule(self.name, 'any', self.ports['launcher2login'], 'tcp', 'allow') # for game servers
+        self.utils.add_rule(self.name, 'any', self.ports['restapi'], 'tcp', 'allow') # for REST
+        self.utils.add_rule(self.name, 'any', self.ports['launcherping'], 'udp', 'allow') # for in-game pings
         self.whitelist.reset()
         self.blacklist.reset()
 
@@ -178,9 +189,15 @@ class Firewall:
 def main():
     print('Running on Python %s' % sys.version)
 
+    config = configparser.ConfigParser()
+    with open(INI_PATH) as f:
+        config.read_file(f)
+
+    ports = Ports(int(config['shared']['port_offset']))
+
     try:
-        udp_proxy_proc1 = sp.Popen('udpproxy.exe %d' % GAME_PORT1)
-        udp_proxy_proc2 = sp.Popen('udpproxy.exe %d' % GAME_PORT2)
+        udp_proxy_proc1 = sp.Popen('udpproxy.exe %d' % ports['gameserver1'])
+        udp_proxy_proc2 = sp.Popen('udpproxy.exe %d' % ports['gameserver2'])
 
     except OSError as e:
         print('Failed to run udpproxy.exe. Run download_udpproxy.py to download it\n'
@@ -190,7 +207,7 @@ def main():
         return
 
     server_queue = gevent.queue.Queue()
-    firewall = Firewall()
+    firewall = Firewall(ports)
     gevent_spawn('firewall.run', firewall.run, server_queue)
 
     def handle_client(socket, address):
@@ -198,7 +215,7 @@ def main():
         command = json.loads(msg.decode('utf8'))
         server_queue.put(command)
 
-    server = StreamServer(('127.0.0.1', 9801), handle_client)
+    server = StreamServer(('127.0.0.1', ports['firewall']), handle_client)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
