152e0754c82933d9cfe7053e26aedba4a34780fc
Maurice van der Pot <griffon26@kfk4ever.com>
2020-02-23 21:19:37 +0100

    Add more hints to serverconfig.lua

--- data/gamesettings/ootb/serverconfig.lua
+++ data/gamesettings/ootb/serverconfig.lua
@@ -6,6 +6,9 @@ ServerSettings.Motd = "This is my OOTB server"
 -- ServerSettings.Password = "some-password"
 ServerSettings.GameSettingMode = ServerSettings.GameSettingModes.OOTB
 
+-- Basic Access Control, see https://www.tamods.org/docs/doc_srv_api_admin.html for more
+-- Admin.Roles.add("admin", "somepasswordyouchoose", true)
+
 ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1", "Light", "Thrust Pack")
 ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1A", "Light", "Thrust Pack")
 ServerSettings.MutuallyExclusiveItems.add("Light", "Phase Rifle", "Light", "Thrust Pack")
@@ -20,3 +23,25 @@ ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1", "Light", "Light Utili
 ServerSettings.MutuallyExclusiveItems.add("Light", "BXT1A", "Light", "Light Utility Pack")
 ServerSettings.MutuallyExclusiveItems.add("Light", "Phase Rifle", "Light", "Light Utility Pack")
 ServerSettings.MutuallyExclusiveItems.add("Light", "SAP20", "Light", "Light Utility Pack")
+
+-- The default map rotation is: Katabatic, ArxNovena, DangerousCrossing, Crossfire, Drydock, Terminus, Sunstar
+-- You can override the default map rotation by uncommenting any of the maps below.
+
+-- ServerSettings.MapRotation.add(Maps.CTF.ArxNovena)
+-- ServerSettings.MapRotation.add(Maps.CTF.BellaOmega)
+-- ServerSettings.MapRotation.add(Maps.CTF.Blueshift)
+-- ServerSettings.MapRotation.add(Maps.CTF.CanyonCrusade)
+-- ServerSettings.MapRotation.add(Maps.CTF.Crossfire)
+-- ServerSettings.MapRotation.add(Maps.CTF.DangerousCrossing)
+-- ServerSettings.MapRotation.add(Maps.CTF.Drydock)
+-- ServerSettings.MapRotation.add(Maps.CTF.Hellfire)
+-- ServerSettings.MapRotation.add(Maps.CTF.IceCoaster)
+-- ServerSettings.MapRotation.add(Maps.CTF.Katabatic)
+-- ServerSettings.MapRotation.add(Maps.CTF.Perdition)
+-- ServerSettings.MapRotation.add(Maps.CTF.Permafrost)
+-- ServerSettings.MapRotation.add(Maps.CTF.Raindance)
+-- ServerSettings.MapRotation.add(Maps.CTF.Stonehenge)
+-- ServerSettings.MapRotation.add(Maps.CTF.Sunstar)
+-- ServerSettings.MapRotation.add(Maps.CTF.Tartarus)
+-- ServerSettings.MapRotation.add(Maps.CTF.TempleRuins)
+-- ServerSettings.MapRotation.add(Maps.CTF.Terminus)
