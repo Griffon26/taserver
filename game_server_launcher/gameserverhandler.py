64e1a39ffb85b59f5c6efbdeaef0e7ac6bf5e2c2
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-11-11 13:46:54 -0800

    remove whitespace change

--- game_server_launcher/gameserverhandler.py
+++ game_server_launcher/gameserverhandler.py
@@ -77,7 +77,7 @@ class GameServerHandler:
         try:
             self.working_dir = game_server_config['dir']
             self.dll_to_inject = game_server_config['controller_dll']
-            self.dll_config_path = os.path.join(data_root, game_server_config['controller_config'])            
+            self.dll_config_path = os.path.join(data_root, game_server_config['controller_config'])
             self.use_external_port = game_server_config.getboolean('use_external_port', True)
         except KeyError as e:
             raise ConfigurationError("%s is a required configuration item under [gameserver]" % str(e))
